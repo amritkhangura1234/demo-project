a6bd0607777f107a757503375ca1eab3be6b8e15
amritkhangura1234 <akhangura@csumb.edu>
2024-05-06 17:19:42 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const URL = 'https://jsonplaceholder.typicode.com/todos';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoDetails, setTodoDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setTodos(data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleTodoClick = async (id) => {
+    try {
+      const response = await fetch(`${URL}/${id}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const details = await response.json();
+      setTodoDetails(details);
+    } catch (error) {
+      console.error('Error fetching todo details: ', error);
+    }
+  };
+
+  //Makes them disappear off the list
+  const handleDeleteAndComplete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+    setTodoDetails(null); 
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div className="todo-list">
+          {todos.map(todo => (
+            <div key={todo.id} onClick={() => handleTodoClick(todo.id)} style={{ cursor: 'pointer' }}>
+              {todo.title}
+            </div>
+          ))}
+        </div>
+        <div className="todo-details">
+          {todoDetails ? (
+            <div>
+              <p><span className='bold'>Title:</span> {todoDetails.title}</p>
+              <p><span className='bold'>User ID:</span> {todoDetails.userId}</p>
+              <button className='delete' onClick={() =>  handleDeleteAndComplete(todoDetails.id)}>Delete</button>
+              <button  className='complete' onClick={() =>  handleDeleteAndComplete(todoDetails.id)}>Completed</button>
+            </div>
+          ) : (
+            <div>Please select a todo to see the details.</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
